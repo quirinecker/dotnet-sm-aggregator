81143dc18433cd146ca26b5a03b09cd12bb0802a
quirinecker <quirin.ecker@bajupa.com>
2025-04-05 18:05:10 +0200

    added basic code for fetching followers of a channel on twitch

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using TwitchLib.Api.Interfaces;
+using TwitchLib.Api;
+
+class Program {
+	static async Task Main(string[] args) {
+		const string clientId = "";
+		const string clientSecret = "";
+		var twitchChannels = new List<string> { "FunkyGamer_12" };
+
+
+		ITwitchAPI api = new TwitchAPI();
+		api.Settings.ClientId = clientId;
+		api.Settings.Secret = clientSecret;
+
+		var response = await api.Helix.Users.GetUsersAsync(logins: twitchChannels);
+
+		if (response.Users.Count() < 1) {
+			Console.WriteLine("No users found");
+		}
+
+		var firstResult = response.Users.First();
+		var email = firstResult.Email == null ? "no email" : firstResult.Email;
+		Console.WriteLine($"Found {firstResult.Id}");
+
+		var followers = await api.Helix.Users.GetUsersFollowsAsync(toId: firstResult.Id, first: 1);
+
+		Console.WriteLine($"{twitchChannels.First()} has {followers.Follows.Count()} followers: ");
+		foreach (var follower in followers.Follows) {
+			Console.WriteLine($"{follower.FromUserName}");
+		}
+	}
+}
