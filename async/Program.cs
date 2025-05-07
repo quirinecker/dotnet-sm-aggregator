2adb3e09d63debf4d5effc5d7337b34c96f0c0d3
quirinecker <quirin.ecker@bajupa.com>
2025-05-07 22:31:50 +0200

    made changes to the cli

--- async/Program.cs
+++ async/Program.cs
@@ -3,54 +3,60 @@
 using TwitchLib.Api;
 using TwitchLib.Api.Helix.Models.Streams.GetStreams;
 using System.Diagnostics;
+using System.Text.Json;
 
-class Program {
+class Program
+{
 
-    private TwitchAPI api = new TwitchAPI();
+	private TwitchAPI api = new TwitchAPI();
 
-	static async Task Main(string[] args) {
-        var program = new Program();
-        await program.run();
-    }
+	static async Task Main(string[] args)
+	{
+		var program = new Program();
+		await program.run();
+	}
 
-    async Task run() {
+	async Task run()
+	{
 		const string clientId = "g79z2bul0nr87ju79sf7jdtc43vbw2";
 		const string clientSecret = "ldhb631jod1fstu0vf4fenu7d6ak0o";
 
-        api.Settings.ClientId = clientId;
+		api.Settings.ClientId = clientId;
 		api.Settings.Secret = clientSecret;
 
 		var stopWatch = new Stopwatch();
 
 		var topGamesRespose = await api.Helix.Games.GetTopGamesAsync();
 
-		foreach (var _ in Enumerable.Range(0, 10)) {
-			var fetchTasks = topGamesRespose.Data
-				.Select(game => api.Helix.Streams.GetStreamsAsync(gameIds: [game.Id], first: 100))
-				.ToList();
+		var fetchTasks = topGamesRespose.Data
+			.Select(game => api.Helix.Streams.GetStreamsAsync(gameIds: [game.Id], first: 100))
+			.ToList();
 
-			await meassure("async", fetchTasks);
-		}
+		await meassure("async", fetchTasks);
+	}
 
-    }
-
-	async Task meassure(string choice, List<Task<GetStreamsResponse>> fetchTasks) {
+	async Task meassure(string choice, List<Task<GetStreamsResponse>> fetchTasks)
+	{
 		var stopWatch = new Stopwatch();
 
 		stopWatch.Start();
-		if (choice == "sync") {
+		if (choice == "sync")
+		{
 			await GetSync(fetchTasks);
 		}
-		else if (choice == "async") {
-			await GetAsync(fetchTasks);
+		else if (choice == "async")
+		{
+			var result = await GetAsync(fetchTasks);
+			Console.WriteLine(JsonSerializer.Serialize(result));
 		}
 		stopWatch.Stop();
 
-		Console.WriteLine($"time: {stopWatch.ElapsedMilliseconds}");
+		Console.Write($"{stopWatch.ElapsedMilliseconds}");
 	}
 
-	async Task<GetStreamsResponse[]> GetSync(List<Task<GetStreamsResponse>> fetchTasks) {
-        var responses = new List<GetStreamsResponse>();
+	async Task<GetStreamsResponse[]> GetSync(List<Task<GetStreamsResponse>> fetchTasks)
+	{
+		var responses = new List<GetStreamsResponse>();
 		foreach (var task in fetchTasks)
 		{
 			responses.Add(await task);
@@ -58,22 +64,26 @@ class Program {
 
 		return responses.ToArray();
 	}
-	async Task<GetStreamsResponse[]> GetAsync(List<Task<GetStreamsResponse>> fetchTasks) {
-        var responses = await Task.WhenAll(tasks: fetchTasks);
+	async Task<GetStreamsResponse[]> GetAsync(List<Task<GetStreamsResponse>> fetchTasks)
+	{
+		var responses = await Task.WhenAll(tasks: fetchTasks);
 		return responses;
 	}
 
-    void PrintResult(GetStreamsResponse[] responses) {
-        var count = 0;
-        foreach (var response in responses) {
-            foreach (var stream in response.Streams) {
-                Console.WriteLine($"({count}) {stream.GameName}: {stream.Title}");
-                count++;
-            }
-            Console.WriteLine("==============================");
-        }
+	void PrintResult(GetStreamsResponse[] responses)
+	{
+		var count = 0;
+		foreach (var response in responses)
+		{
+			foreach (var stream in response.Streams)
+			{
+				Console.WriteLine($"({count}) {stream.GameName}: {stream.Title}");
+				count++;
+			}
+			Console.WriteLine("==============================");
+		}
 
-        Console.WriteLine($"total: {count}");
-    }
+		Console.WriteLine($"total: {count}");
+	}
 
 }
--- benchmark-cli/Program.cs
+++ benchmark-cli/Program.cs
@@ -1,26 +1,78 @@
 ﻿using System.Diagnostics;
+using System.Text.Json;
+
+class Config
+{
+	public required List<string> Exec { get; set; }
+	public required List<string> ExecWarmup { get; set; }
+	public required List<string> Stages { get; set; }
+}
 
 class Program
 {
 	static void Main(string[] args)
 	{
-		if (args.Length == 0 || args.Length > 2) {
-			Console.WriteLine("Usage: benchmark-cli <path-to-executable> <?intermediates>");
+		if (args.Length != 1) {
+			Console.WriteLine("Usage: benchmark-cli <config>");
 			return;
 		}
 
-		var exec_arg = args[0].Split(" ").ToList();
-		var exec = exec_arg.First();
-		var arguments = exec_arg.Skip(1).ToList();
+		var config = ReadConfig(args[0]);
+
+		if (config == null) {
+			Console.WriteLine("Could not read config file. It might not be in the required format or does not exist");
+			return;
+		}
+
+		var intermediates = config.Stages;
 		var results = new List<Tuple<List<int>, long>>();
 
 		for (var i = 0; i < 10; i++) {
-			var result = RunProcess(exec, arguments);
+			var (executable, arguments) = ParseExec(config.ExecWarmup);
+			RunProcess(executable, arguments);
+		}
+
+		for (var i = 0; i < 10; i++) {
+			var (executable, arguments) = ParseExec(config.Exec);
+			var result = RunProcess(executable, arguments);
 			Console.WriteLine($"time: {result.Item2}");
 			results.Add(result);
 		}
 
-		Console.WriteLine($"Avg Time: {resu}")
+		Tuple<List<int>, long> accumulator = Tuple.Create(new List<int>(), 0L);
+		var finalResult = results.Aggregate(accumulator,(acc,y) => {
+			foreach (var (item, index) in y.Item1.Select((item, index) => (item, index))) {
+				if (acc.Item1.Count() <= index) {
+					acc.Item1.Add(0);
+				}
+				acc.Item1[index] += item;
+			}
+
+			return Tuple.Create(acc.Item1, acc.Item2 + y.Item2);
+		});
+
+		var averagedResult = Tuple.Create(finalResult.Item1.Select(x => (float) x / 10).ToList(), (float) finalResult.Item2 / 10 );
+
+		Console.WriteLine($"Avg Time: {finalResult.Item2}/{10}");
+		Console.WriteLine($"Avg Time: {averagedResult.Item2}");
+
+		if (intermediates == null) {
+			return;
+		}
+
+		foreach (var ( result, intermediate) in averagedResult.Item1.Zip(intermediates))
+		{
+			Console.WriteLine($"{intermediate}: {result}");
+		}
+	}
+
+	static (string, List<string>) ParseExec(List<string> exec) {
+		return (exec.First(), exec.Skip(1).ToList());
+	}
+
+	static Config? ReadConfig(string path) {
+		string jsonString = File.ReadAllText(path);
+		return JsonSerializer.Deserialize<Config>(jsonString);
 	}
 
 	static Tuple<List<int>, long> RunProcess(string exec, List<string> arguments) {
